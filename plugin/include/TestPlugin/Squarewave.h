568efe9089f0b2b6279f180b46c3ed43e558b921
gaslam <44768183+gaslam@users.noreply.github.com>
2025-11-28 12:25:02 +0100

    Smoothen frequency in square

--- plugin/include/TestPlugin/Squarewave.h
+++ plugin/include/TestPlugin/Squarewave.h
@@ -1,24 +1,25 @@
 ﻿#ifndef SQUAREWAVECHANNEL_H
 #define SQUAREWAVECHANNEL_H
+#include <juce_audio_basics/juce_audio_basics.h>
+
 #include <cmath>
 
 class Squarewave {
 public:
   void prepare(double sampleRate);
-  void process(float* output,const int numChannels);
+  void process(float* output,const int numSamples);
 
   [[nodiscard]] float getAmplitude() const {return m_Amplitude;};
-  [[nodiscard]] float getFrequency() const {return m_Frequency;};
+  [[nodiscard]] float getFrequency() const {return m_SmoothedFreq.getCurrentValue();};
 
   void setAmplitude(const float amplitude) {
     m_Amplitude = amplitude;
-    m_Frequency = m_Amplitude / 2.f;
   };
-  void setFrequency(const float frequency) {m_Frequency = frequency;};
+  void setFrequency(const float frequency) {m_SmoothedFreq.setTargetValue( frequency);}
 
 private:
   float m_Amplitude = 0.2f;
-  float m_Frequency = 440.0f;
+  juce::SmoothedValue<float,juce::ValueSmoothingTypes::Multiplicative> m_SmoothedFreq;
   float m_CurrentSampleRate = 0.0f;
   float m_TimeIncrement = 0.0f;
   float m_CurrentTime{};
--- plugin/source/Squarewave.cpp
+++ plugin/source/Squarewave.cpp
@@ -1,17 +1,27 @@
 ﻿#include "TestPlugin/Squarewave.h"
 #include <numbers>
+
 void Squarewave::prepare(double sampleRate) {
-  m_TimeIncrement = 1.f / static_cast<float>(sampleRate);
+  const float sampleRateFl{static_cast<float>(sampleRate)};
+  m_TimeIncrement = 1.f / sampleRateFl;
+  m_CurrentSampleRate = sampleRateFl;
+  m_SmoothedFreq.reset(sampleRate,.1f);
+  m_SmoothedFreq.setCurrentAndTargetValue(220.f);
 }
 
+void Squarewave::process(float* output, const int numSamples) {
+  constexpr float piTwo = 2.0f * std::numbers::pi_v<float>;
+  for (int sample{}; sample < numSamples; ++sample) {
+    float frequency { m_SmoothedFreq.getNextValue()};
+    const float phaseIncrease{(piTwo * frequency) / m_CurrentSampleRate};
+    const float sine{std::sinf(m_CurrentTime)};
+    const float sign{sine > 0.0f ? 1.0f : -1.0f};
 
-void Squarewave::process(float* output,const int numChannels) {
-  constexpr float piTwo = 2.0f*std::numbers::pi_v<float>;
-  for (int sample{}; sample < numChannels; ++sample) {
-    const float sine{std::sinf(piTwo*m_Frequency * m_CurrentTime)};
-    const float sign { sine > 0 ? 1.0f : -1.0f };
-    const float value = m_Amplitude * sign;
-output[sample] = value;
-    m_CurrentTime += m_TimeIncrement;
+    output[sample] = sign * m_Amplitude;
+    m_CurrentTime += phaseIncrease;
+
+    if (m_CurrentTime > piTwo) {
+      m_CurrentTime -= piTwo;
+    }
   }
 }
